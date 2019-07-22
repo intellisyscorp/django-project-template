21154273544422c78c014590e19ad84e34c48d1d
Jethro Lee <dlemfh96@gmail.com>
2019-07-22 17:17:07 +0900

    Remove unnecessary import from template

--- app/views/v1/urls.py
+++ app/views/v1/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_jwt.views import verify_jwt_token, refresh_jwt_token
 
 
 schema_view = get_schema_view(
