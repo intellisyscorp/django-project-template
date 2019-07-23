b1d8174d7cb22c1ccfdef802a6dbb1f072a579cb
한지승 <jinh574@naver.com>
2019-07-23 19:02:13 +0900

    Add static folder config

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -157,5 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
