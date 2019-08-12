ad5f82c1194cd1dc3aacf1f270f574fd35907fc5
Jethro Lee <dlemfh96@gmail.com>
2019-08-12 17:33:10 +0900

    Update initialize_devpod.sh

--- scripts/initialize_devpod.sh
+++ scripts/initialize_devpod.sh
@@ -16,7 +16,7 @@ if test -z "$DEVPOD_NAME"
 then
     echo "This is not a devpod"
 else
-    PGPASSWORD=test psql --host develop-service-db -U test -c "DROP DATABASE \"$HOSTNAME_{{ project_name }}\""
-    PGPASSWORD=test psql --host develop-service-db -U test -c "CREATE DATABASE \"$HOSTNAME_{{ project_name }}\""
+    PGPASSWORD=test psql --host develop-service-db -U test -c "DROP DATABASE \"$HOSTNAME-{{ project_name }}\""
+    PGPASSWORD=test psql --host develop-service-db -U test -c "CREATE DATABASE \"$HOSTNAME-{{ project_name }}\""
     python manage.py migrate
 fi
