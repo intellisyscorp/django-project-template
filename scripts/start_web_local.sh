6a70417ab2a0bd03798edb0726646bb723a0a480
Jethro Lee <dlemfh96@gmail.com>
2019-08-13 11:14:40 +0900

    Update start_web_local.sh

--- scripts/start_web_local.sh
+++ scripts/start_web_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.base"
+export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.base
 
 echo "Setting: ${DJANGO_SETTINGS_MODULE}"
 
@@ -10,4 +10,4 @@ python manage.py migrate
 
 echo "Run server..."
 # python manage.py runserver 0.0.0.0:8000 --verbosity 0
-gunicorn weather.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 1
+gunicorn {{ project_name }}.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 1
