08a1db6d3a3ea772b14480d85fcd31d9533a39c4
Jethro Lee <dlemfh96@gmail.com>
2019-08-14 18:27:53 +0900

    Edit allowed hosts; requirements.txt

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['fitzme-gateway.jx-production.fitzme.xyz', 'localhost']
+ALLOWED_HOSTS = ['fitzme-gateway.jx-production.fitzme.xyz', 'localhost', '127.0.0.1']
--- project_name/settings/staging.py
+++ project_name/settings/staging.py
@@ -2,6 +2,6 @@ from .production import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['fitzme-gateway.jx-staging.fitzme.xyz', 'localhost']
+ALLOWED_HOSTS = ['fitzme-gateway.jx-staging.fitzme.xyz', 'localhost', '127.0.0.1']
 
 MIDDLEWARE += ['app.middlewares.debug_middleware']
