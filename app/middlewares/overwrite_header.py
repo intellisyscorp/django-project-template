a9567229b8fffc6a1c90ea9ebe4e9003851ca087
Jethro Lee <dlemfh96@gmail.com>
2019-08-14 09:58:06 +0900

    Update overwrite_header.py

--- app/middlewares/overwrite_header.py
+++ app/middlewares/overwrite_header.py
@@ -2,7 +2,7 @@
 
 def overwrite_header(get_response):
     def middleware(request):
-        if 'X-Gateway-Name' in request.headers and request.headers['X-Gateway-Name'] == 'fitzme-gateway':
+        if request.headers.get('X-Gateway-Name') == 'fitzme-gateway':
             request.META['HTTP_X_FORWARDED_PROTO'] = 'https'
 
         response = get_response(request)
