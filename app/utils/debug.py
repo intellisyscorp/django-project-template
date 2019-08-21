e782bc05ac69fa7692b8e5c169078d2e24c52f85
Jethro Lee <dlemfh96@gmail.com>
2019-08-21 12:07:26 +0900

    Update debug.py

--- app/utils/debug.py
+++ app/utils/debug.py
@@ -9,16 +9,16 @@ __all__ = [
 
 
 def debug_db_queries_here(marker: str = 'HERE'):
-    print(f'>>> # of Queries {marker}: {len(connection.queries)}')
+    print(f'\t# of Queries {marker}: {len(connection.queries)}')
 
 
 def debug_db_queries_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print(f'>>> # of Queries BEFORE {func}: {len(connection.queries)}')
+        print(f'\t# of Queries BEFORE {func}: {len(connection.queries)}')
 
         response = func(*args, **kwargs)
 
-        print(f'>>> # of Queries AFTER {func}: {len(connection.queries)}')
+        print(f'\t# of Queries AFTER {func}: {len(connection.queries)}')
         return response
     return wrapper
