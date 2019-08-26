2e0a84590ec19755743a0aca53526893865a3278
Jethro Lee <dlemfh96@gmail.com>
2019-08-26 14:42:49 +0900

    Update production.py

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -5,3 +5,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['fitzme-gateway.jx-production.fitzme.xyz', 'localhost', '127.0.0.1']
 
 LOGGING['loggers']['']['level'] = 'INFO'
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+    # 'rest_framework.renderers.BrowsableAPIRenderer',
+)
