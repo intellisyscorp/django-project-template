585b9c047759436340e797bd660fd9026519f4fa
Jethro Lee <dlemfh96@gmail.com>
2019-08-26 14:43:48 +0900

    Update staging.py

--- project_name/settings/staging.py
+++ project_name/settings/staging.py
@@ -7,3 +7,8 @@ ALLOWED_HOSTS = ['fitzme-gateway.jx-staging.fitzme.xyz', 'localhost', '127.0.0.1
 LOGGING['loggers']['']['level'] = 'DEBUG'
 
 MIDDLEWARE += ['app.middlewares.debug_middleware']
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+    'rest_framework.renderers.BrowsableAPIRenderer',
+)
