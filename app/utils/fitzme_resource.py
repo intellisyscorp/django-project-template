e48c9d878fa1a210c621ac65b0cd756969f4160a
Jethro Lee <dlemfh96@gmail.com>
2019-09-05 11:02:02 +0900

    Include fitzme_read_only_resource decorator

--- app/utils/fitzme_resource.py
+++ app/utils/fitzme_resource.py
@@ -1,5 +1,5 @@
 from django.db.models import Model
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 
@@ -38,6 +38,29 @@ def fitzme_resource(ResourceModel: Model):
         @method_decorator(name='destroy', decorator=swagger_auto_schema(
             operation_id=f'{ModelNameEn} destroy',
             operation_description=f'특정 {ModelNameKo} 정보를 삭제합니다.',
+            tags=[f'Resource {ModelNameEn}'],
+        ))
+        class ResourceViewSetWrapper(ResourceViewSet):
+            pass
+
+        return ResourceViewSetWrapper
+    return inner
+
+
+def fitzme_read_only_resource(ResourceModel: Model):
+    def inner(ResourceViewSet: ReadOnlyModelViewSet):
+
+        ModelNameEn = ResourceModel.__name__
+        ModelNameKo = ResourceModel._meta.verbose_name
+
+        @method_decorator(name='list', decorator=swagger_auto_schema(
+            operation_id=f'{ModelNameEn} list',
+            operation_description=f'{ModelNameKo} 정보 리스트를 반환합니다.',
+            tags=[f'Resource {ModelNameEn}'],
+        ))
+        @method_decorator(name='retrieve', decorator=swagger_auto_schema(
+            operation_id=f'{ModelNameEn} retrieve',
+            operation_description=f'특정 {ModelNameKo} 정보를 반환합니다.',
             tags=[f'Resource {ModelNameEn}'],
         ))
         class ResourceViewSetWrapper(ResourceViewSet):
