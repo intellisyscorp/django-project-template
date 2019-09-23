2a8a1f524d526eb616b8f778879fb862fd0a1837
한지승 <jinh574@naver.com>
2019-09-23 16:13:03 +0900

    Update production.py

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['fitzme-gateway.jx-production.fitzme.xyz', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fitzme-gateway.jx-production.fitzme.co.kr', 'localhost', '127.0.0.1']
 
 LOGGING['loggers']['']['level'] = 'INFO'
 
