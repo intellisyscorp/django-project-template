9aa767d2c5379bfda4add0d18ed8ab2c174167ea
Jethro Lee <dlemfh96@gmail.com>
2019-10-24 12:02:56 +0900

    Update initialize_devpod.sh

--- scripts/initialize_devpod.sh
+++ scripts/initialize_devpod.sh
@@ -4,7 +4,7 @@ yum -y install gcc gcc-c++ curl bzip2 postgresql vim \
     && curl -sSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh \
     && /bin/sh /tmp/miniconda.sh -bfp /usr/local/ \
     && rm -rf /tmp/miniconda.sh \
-    && conda install -y python=3.7.3
+    && conda install -y python=3.7.3 pip
 
 pip install --upgrade pip awscli awscli-local
 pip install cython \
